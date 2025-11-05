27cd6871ade667ea06cc803ec87b45e66f07facf
Dheena Krishna <dheenakrishna2020@gmail.com>
2025-11-06 00:45:20 +0530

    Hello world for Spring boot

---
+++ spring-boot-hello-world/src/test/java/com/tnsif/ifet/spring_boot_hello_world/SpringBootHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.ifet.spring_boot_hello_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
